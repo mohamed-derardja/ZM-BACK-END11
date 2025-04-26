240e69ef6b244e456d7f9fec9ded3e75ba51b3cb
CorTec <truemoh40@gmail.com>
2025-04-26 21:58:32 +0100

    26/04/2025--creation:21:57pm.

---
+++ src/main/java/com/zm/zmbackend/ZmBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.zm.zmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZmBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zm/zmbackend/ZmBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zm.zmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
