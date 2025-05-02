292a85d84c3a0d788d17802301bb596baf273710
CorTec <truemoh40@gmail.com>
2025-05-02 10:39:43 +0100

    01/05/2025--depencies_resolved:10:39am.

--- src/main/java/com/zm/zmbackend/ZmBackEndApplication.java
+++ src/main/java/com/zm/zmbackend/ZmBackEndApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZmBackEndApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ZmBackEndApplication.class, args);
     }
 
